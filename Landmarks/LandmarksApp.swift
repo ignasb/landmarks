bf643a69215931b40852d600116c0fcdca30308f
Ignas Burlingis <ignas.burlingis@gmail.com>
2023-04-09 19:58:01 +0300

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Ignas Burlingis on 2023-04-09.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
